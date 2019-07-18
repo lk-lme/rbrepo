e5e7d5c783f0d6fc5c9c320ffc548b296060505b
Jay Freestone <jay.freestone@red-badger.com>
2019-07-18 12:16:09 +0100

    feat(Button): Add link functionality.

--- packages/pattern-library/src/components/Button/index.tsx
+++ packages/pattern-library/src/components/Button/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import buttonStyles from './button.scss';
 
-const Button: React.FunctionComponent<Props> = ({ children }) => (
-  <button className={buttonStyles.btn}>{children}</button>
-);
+const Button: React.FunctionComponent<Props> = ({
+  url,
+  children,
+}) => {
+  const El = url ? 'a' : 'button';
+
+  return (
+    <El
+      className={buttonStyles.btn}
+      href={url}
+    >
+      {children}
+    </El>
+  );
+};
 
 interface Props {
   onClick?(): void;
-  href?: string;
+  url?: string;
 }
 
 export default Button;
