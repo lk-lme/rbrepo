85eed4ad1d878006bee99398bddfb5005e173b43
Jay Freestone <jay.freestone@red-badger.com>
2019-07-30 15:34:26 +0100

    feat(DefinitionList): Add component.

---
+++ packages/pattern-library/src/components/DefinitionList/definition-list.scss.d.ts
@@ -0,0 +1,7 @@
+declare const styles: {
+  readonly "definition-list": string;
+  readonly "definition-list__title": string;
+  readonly "definition-list__description": string;
+};
+export = styles;
+
