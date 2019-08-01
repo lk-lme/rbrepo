8cd5e1651fa5b6c31756669379de1c55bb663297
Jay Freestone <jay.freestone@red-badger.com>
2019-08-01 11:04:01 +0100

    fix(StepByStep): Resolve CSS typing issue.

--- packages/pattern-library/src/components/StepByStep/step-by-step.scss.d.ts
+++ packages/pattern-library/src/components/StepByStep/step-by-step.scss.d.ts
@@ -1,7 +1,7 @@
 declare const styles: {
   readonly "step-by-step__title": string;
-  readonly "step-by-step__item": string;
   readonly "step-by-step__toggle": string;
+  readonly "step-by-step__item": string;
 };
 export = styles;
 
