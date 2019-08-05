9b94663081bb058608fdc5eb0117957d5bb53abc
Jay Freestone <jay.freestone@red-badger.com>
2019-08-05 14:46:36 +0100

    feat(Step): Re-create step to match design.

--- packages/pattern-library/src/components/Steps/index.tsx
+++ packages/pattern-library/src/components/Steps/index.tsx
@@ -2,64 +2,48 @@ import React from 'react';
 import cx from 'classnames';
 import styles from './steps.scss';
 import Link from './../Link';
-import CheckIcon from './../../svg/check-circle.svg';
+import Icon from './../Icon';
+import CheckIcon from './../../svg/circle-tick.svg';
 
 const iconMap = {
   success: CheckIcon,
 };
 
-const Steps: React.FunctionComponent<Props> = ({ steps, activeID }) => {
-  return (
-    <div>
-      {renderSteps(steps, activeID)}
-    </div>
-  );
-};
-
-function isParentOfActive(steps: StepType[], activeID?: string): boolean {
-  return steps.some((step) => {
-    if (step.id === activeID) return true;
-    if (step.steps) return isParentOfActive(step.steps, activeID);
-    return false;
-  });
-}
-
-function renderSteps(sections: StepType[], activeID?: string) {
-  if (!sections) return null;
-
-  return (
-    <ol className={styles['steps__section']}>
-      {sections.map(({ id, steps, title, status }) => (
-        <li
-          className={cx(styles['steps__step'], {
-            [styles['steps__step--is-active']]: activeID === id || (steps ? isParentOfActive(steps, activeID) : null),
-          })}
-        >
-          {/* @todo: add link href etc */}
-          <Link className={styles['steps__step-title']}>
-            <span className={styles['steps__step-status']}>
+const Steps: React.FunctionComponent<Props> = ({ steps, activeID }) => (
+  <ol className={styles.steps}>
+    {steps.map(({ id, title, status }) => (
+      <li
+        className={cx(styles['steps__step'],  {
+          [styles['steps__step--success']]: status === 'success',
+          [styles['steps__step--is-active']]: activeID === id,
+        })}
+      >
+        {/* @todo: add link href etc */}
+        <Link className={styles['steps__step-title']}>
+          <span className={styles['steps__step-status']}>
+            <span className={styles['steps__step-indicator']}>
               {(() => {
                 // @todo Refactor
                 const StatusIcon = iconMap[status];
                 if (!StatusIcon) return null;
-                return <StatusIcon className={styles['steps__step-icon']} />;
+                return <Icon className={styles['steps__step-icon']} component={StatusIcon} />;
               })()}
             </span>
+          </span>
+          <span className={styles['steps__step-label']}>
             {title}
-          </Link>
-          {steps ? renderSteps(steps, activeID) : null}
-        </li>
-      ))}
-    </ol>
-  );
-}
+          </span>
+        </Link>
+      </li>
+    ))}
+  </ol>
+);
 
 type StepType = {
   id: string;
   title: string;
   link: any;
   status?: 'success'|'neutral'|'warning';
-  steps?: StepType[],
 };
 
 interface Props {
