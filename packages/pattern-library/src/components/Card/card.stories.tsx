9d0e2458589f48f9af0b3edcf3e8d8c3178b4227
Jay Freestone <jay.freestone@red-badger.com>
2019-08-05 09:06:10 +0100

    docs(Card): Add readme.

--- packages/pattern-library/src/components/Card/card.stories.tsx
+++ packages/pattern-library/src/components/Card/card.stories.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { addReadme } from 'storybook-readme';
 import { storiesOf } from '@storybook/react';
 import Card from '.';
+import CardReadme from './README.md';
 
 storiesOf('Card', module)
+  .addDecorator(addReadme)
+  .addParameters({
+    readme: {
+      sidebar: CardReadme,
+    },
+  })
   .add('default', () => (
     <div style={{ padding: '2rem' }}>
       <Card>
