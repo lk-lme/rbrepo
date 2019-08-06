72f72490e64aaf25bee99dd85db633cb371f589b
Jay Freestone <jay.freestone@red-badger.com>
2019-08-06 11:29:15 +0100

    feat(PageHeader): Improve resiliance, add stories.

--- packages/pattern-library/src/components/PageHeader/page-header.stories.tsx
+++ packages/pattern-library/src/components/PageHeader/page-header.stories.tsx
@@ -7,8 +7,63 @@ storiesOf('PageHeader', module)
     <div style={{ padding: '2rem' }}>
       <PageHeader
         title="Contract: AH Future"
+      />
+    </div>
+  ))
+  .add('with action', () => (
+    <div style={{ padding: '2rem' }}>
+      <PageHeader
+        title="Contract: AH Future"
+        actions={[ { title: 'Clone' } ]}
+      />
+    </div>
+  ))
+  .add('with badge', () => (
+    <div style={{ padding: '2rem' }}>
+      <PageHeader
+        title="Contract: AH Future"
+        badges={[{ title: 'In review', type: 'warning' }]}
+      />
+    </div>
+  ))
+  .add('with badge and actions', () => (
+    <div style={{ padding: '2rem' }}>
+      <PageHeader
+        title="Contract: AH Future"
         badges={[{ title: 'Draft', type: 'primary' }]}
         actions={[
+          {
+            title: 'Clone',
+          },
+          {
+            title: 'Export',
+          },
+          {
+            title: 'Delete',
+          },
+        ]}
+      />
+    </div>
+  ))
+  .add('stress test', () => (
+    <div style={{ padding: '2rem' }}>
+      <PageHeader
+        title="Contract: AH Future Contract AH Future"
+        badges={[
+          { title: 'Draft', type: 'primary' },
+          { title: 'Secondary', type: 'warning' },
+          { title: 'Review', type: 'success' },
+        ]}
+        actions={[
+          {
+            title: 'Clone',
+          },
+          {
+            title: 'Export',
+          },
+          {
+            title: 'Delete',
+          },
           {
             title: 'Clone',
           },
