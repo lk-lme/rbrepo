c866caaadeed446943af0de4ae7daa1c5607378b
Jay Freestone <jay.freestone@red-badger.com>
2019-08-09 14:40:11 +0100

    feat(Button): Add size option.

--- packages/pattern-library/src/components/Button/button.scss.d.ts
+++ packages/pattern-library/src/components/Button/button.scss.d.ts
@@ -3,6 +3,8 @@ declare const styles: {
   readonly "btn--naked": string;
   readonly "btn--secondary": string;
   readonly "btn--outline": string;
+  readonly "btn--sm": string;
+  readonly "btn--lg": string;
 };
 export = styles;
 
--- packages/pattern-library/src/components/Button/index.tsx
+++ packages/pattern-library/src/components/Button/index.tsx
@@ -5,6 +5,7 @@ import buttonStyles from './button.scss';
 const Button: React.FunctionComponent<Props & React.ButtonHTMLAttributes<Props>> = ({
   url,
   variety,
+  size,
   disabled,
   children,
   className,
@@ -16,6 +17,7 @@ const Button: React.FunctionComponent<Props & React.ButtonHTMLAttributes<Props>>
     <El
       className={cx(buttonStyles.btn, className, {
         [buttonStyles[`btn--${variety}`]]: variety && variety !== 'normal',
+        [buttonStyles[`btn--${size}`]]: size && size !== 'md',
       })}
       href={url}
       ref={ref}
@@ -29,14 +31,17 @@ const Button: React.FunctionComponent<Props & React.ButtonHTMLAttributes<Props>>
 
 Button.defaultProps = {
   variety: 'primary',
+  size: 'md',
 };
 
 export type Variety = 'primary'|'secondary'|'outline'|'naked';
+export type Size = 'sm'|'md'|'lg';
 
-interface Props {
+export interface Props {
   onClick?(e: Event): void;
   onKeyDown?(e: KeyboardEvent): void;
   variety?: Variety;
+  size?: Size;
   disabled?: boolean;
   url?: string;
   tabIndex?: number;
