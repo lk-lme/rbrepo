140c84d0ea1bea53ec81f03998eedb37f1a15224
Jay Freestone <jay.freestone@red-badger.com>
2019-08-13 16:49:13 +0100

    fix(PaddingDecorator): Allow for no options.

--- packages/pattern-library/.storybook/decorators/PaddingDecorator.tsx
+++ packages/pattern-library/.storybook/decorators/PaddingDecorator.tsx
@@ -5,7 +5,7 @@ const styles = { padding: '2rem' };
 /**
  * A StoryBook decorator that adds a bit of padding around a component. 
  */
-const PaddingDecorator = ({ withBG }: { withBG: boolean }) => storyFn => (
+const PaddingDecorator = ({ withBG }: { withBG?: boolean } = {}) => storyFn => (
   <div
     style={{
       padding: '2rem',
