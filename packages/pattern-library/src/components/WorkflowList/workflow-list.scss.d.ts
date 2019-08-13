673c1ccee65d33b33f1b8909f55daa892f380a7b
Jay Freestone <jay.freestone@red-badger.com>
2019-08-13 20:53:29 +0100

    feat(WorkflowList): Add component.

---
+++ packages/pattern-library/src/components/WorkflowList/workflow-list.scss.d.ts
@@ -0,0 +1,14 @@
+declare const styles: {
+  readonly "list": string;
+  readonly "list__item": string;
+  readonly "step": string;
+  readonly "step--sub": string;
+  readonly "step--danger": string;
+  readonly "step--success": string;
+  readonly "step__heading": string;
+  readonly "step__deadline": string;
+  readonly "step__status": string;
+  readonly "substep": string;
+};
+export = styles;
+
---
+++ packages/pattern-library/src/components/WorkflowList/workflow-list.stories.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import PaddingDecorator from 'Decorators/PaddingDecorator';
+import WorkflowList from '.';
+
+storiesOf('WorkflowList', module)
+  .addDecorator(PaddingDecorator())
+  .add('basic', () => (
+    <WorkflowList
+      stages={[
+        {
+          id: 'foureyes',
+          title: 'Send for 4-eyes check',
+          deadline: '2019-03-19',
+          status: {
+            type: 'neutral',
+            label: 'Waiting',
+          },
+          dependencies: [
+            {
+              id: 'checkerone',
+              title: 'Checker one',
+              completed: '2019-03-19',
+              status: {
+                type: 'success',
+                label: 'Done',
+              },
+            },
+            {
+              id: 'checkertw',
+              title: 'Checker two',
+              deadline: '2019-03-19',
+              status: {
+                type: 'danger',
+                label: 'Rejected',
+              },
+            }
+          ],
+        },
+        {
+          id: 'preprod',
+          title: 'Load onto Pre-production environement',
+          deadline: '2019-03-13',
+          status: {
+            type: 'neutral',
+            label: 'Waiting',
+          },
+        },
+        {
+          id: 'checks',
+          title: 'Conduct pre-production checks (PTO)',
+          deadline: '2019-03-13',
+          status: {
+            type: 'neutral',
+            label: 'Waiting',
+          },
+        },
+        {
+          id: 'feedback',
+          title: 'Request stackeholder feedback',
+          deadline: '2019-03-14',
+          status: {
+            type: 'neutral',
+            label: 'Waiting',
+          },
+          dependencies: [
+            {
+              id: 'stackeholderone',
+              title: 'Stakeholder one',
+              deadline: '2019-03-14',
+              status: {
+                type: 'neutral',
+                label: 'Waiting',
+              },
+            },
+            {
+              id: 'stackeholdertwo',
+              title: 'Stakeholder one',
+              deadline: '2019-03-14',
+              status: {
+                type: 'neutral',
+                label: 'Waiting',
+              },
+            }
+          ]
+        },
+        {
+          id: 'productionload',
+          title: 'Load onto production environment',
+          deadline: '2019-03-16',
+          status: {
+            type: 'neutral',
+            label: 'Waiting',
+          },
+        },
+        {
+          id: 'confirmchanges',
+          title: 'Confirm changes',
+          deadline: '2019-03-16',
+          status: {
+            type: 'neutral',
+            label: 'Waiting',
+          },
+        }
+      ]}
+    />
+  ));
