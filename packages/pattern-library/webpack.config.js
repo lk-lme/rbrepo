93d3877800b5902b8317e967fc9d83197507a930
Jay Freestone <jay.freestone@red-badger.com>
2019-08-13 20:56:08 +0100

    chore(ide): Add dummy webpack config for Intellij.

---
+++ packages/pattern-library/webpack.config.js
@@ -0,0 +1,17 @@
+/**
+ * Dummy webpack config for IntelliJ path resolution
+ */
+module.exports = {
+  context: __dirname,
+  resolve: {
+    alias: {
+      Components: path.resolve(__dirname, '../src/components/'),
+      Hooks: path.resolve(__dirname, '../src/hooks/'),
+      HOC: path.resolve(__dirname, '../src/hoc/'),
+      SVG: path.resolve(__dirname, '../src/svg/'),
+      Utils: path.resolve(__dirname, '../src/utils/'),
+      Styles: path.resolve(__dirname, '../src/styles/'),
+      Decorators: path.resolve(__dirname, './decorators'),
+    },
+  }
+};
