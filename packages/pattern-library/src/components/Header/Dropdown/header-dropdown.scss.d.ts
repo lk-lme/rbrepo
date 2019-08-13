48e81fd05ae04bdf822f81ceca598db707531537
Jay Freestone <jay.freestone@red-badger.com>
2019-08-13 16:55:01 +0100

    feat(Header): Add basic notifications dropdown.

---
+++ packages/pattern-library/src/components/Header/Dropdown/header-dropdown.scss.d.ts
@@ -0,0 +1,7 @@
+declare const styles: {
+  readonly "dropdown": string;
+  readonly "toggle": string;
+  readonly "toggle--is-active": string;
+};
+export = styles;
+
--- packages/pattern-library/src/components/Header/index.tsx
+++ packages/pattern-library/src/components/Header/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import cx from 'classnames';
 import Form from './../Form';
 import TextInput from './../TextInput';
 import Button from './../Button';
 import Link from './../Link';
 import FormField from './../FormField';
+import Notifications, { NotificationType } from './../Notifications';
+import HeaderDropdown from './Dropdown/index';
 import Icon from './../Icon';
+import BellIcon from './../../svg/bell.svg';
 import SearchIcon from './../../svg/search.svg';
 import Logo from './../../svg/lme-logo-inverted.svg';
 import styles from './header.scss';
@@ -12,6 +16,10 @@ import visuallyHidden from './../../styles/utilities/_visually-hidden.scss';
 
 const Header: React.FunctionComponent<Props> = ({
   user,
+  notifications = {
+    items: [],
+    hasUnread: false,
+  },
 }) => (
   <header className={styles.header}>
     <Link className={styles.brand}>
@@ -47,6 +55,18 @@ const Header: React.FunctionComponent<Props> = ({
         </form>
       )}
     </Form>
+    <HeaderDropdown
+      label={(
+        <div className={cx(styles['secondary-action'], notifications.hasUnread && styles['secondary-action--has-unread'])}>
+          <Icon component={BellIcon} />
+          <span>Notifications</span>
+        </div>
+      )}
+    >
+      {notifications.items && (
+        <Notifications items={notifications.items} />
+      )}
+    </HeaderDropdown>
     {user && (
       <Link className={styles.profile}>
         <img className={styles['profile__image']} src={user.avatar} alt="" />
@@ -59,6 +79,10 @@ const Header: React.FunctionComponent<Props> = ({
 );
 
 interface Props {
+  notifications?: {
+    items: NotificationType[];
+    hasUnread?: boolean;
+  };
   user?: {
     id: string;
     name: string;
